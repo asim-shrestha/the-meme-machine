826fb412df29f1351a6fcac57b9d974f7182bced
Asim Shrestha <ashresth@sfu.ca>
2021-02-13 13:18:10 -0800

    feat: add container to meme list

---
+++ frontend/util/data.js
@@ -0,0 +1,52 @@
+export const ALL_MEMES = [
+  {
+    image: "http://i.imgflip.com/1bij.jpg",
+    stars: 20,
+    comments: 10,
+  },
+  {
+    image: "https://cdn.discordapp.com/attachments/770055809052180504/810226627228598282/9141b8e7-cfcb-46a5-9e3a-5f946f547edd.png",
+    stars: 250,
+    comments: 150,
+  },
+  {
+    image: "https://i.chzbgr.com/full/8944133/h126EBC4E/funny-memes-about-coding-and-programming",
+    stars: 50,
+    comments: 2,
+  },
+  {
+    image: "https://i.redd.it/isyu86r7sor21.png",
+    stars: 2,
+    comments: 0,
+  },
+  {
+    image: "https://cdn.discordapp.com/attachments/770055809052180504/810226627228598282/9141b8e7-cfcb-46a5-9e3a-5f946f547edd.png",
+    stars: 250,
+    comments: 150,
+  },
+  {
+    image: "http://i.imgflip.com/1bij.jpg",
+    stars: 20,
+    comments: 10,
+  },
+  {
+    image: "https://i.redd.it/isyu86r7sor21.png",
+    stars: 2,
+    comments: 0,
+  },
+  {
+    image: "https://i.redd.it/isyu86r7sor21.png",
+    stars: 2,
+    comments: 0,
+  },
+  {
+    image: "https://i.chzbgr.com/full/8944133/h126EBC4E/funny-memes-about-coding-and-programming",
+    stars: 50,
+    comments: 2,
+  },
+  {
+    image: "https://i.redd.it/isyu86r7sor21.png",
+    stars: 2,
+    comments: 0,
+  },
+]
