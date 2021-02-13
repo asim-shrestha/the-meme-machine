64a53d69393ec2351a11d0b6013325ff77234d83
Asim Shrestha <ashresth@sfu.ca>
2021-02-13 13:05:39 -0800

    🃏 Add meme card and meme card list

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,12 @@
 import AppNavBar from '../components/AppNavBar';
+import MemeCard from '../components/MemeCard';
+import MemeList from '../components/MemeList';
 
 export default function Home() {
   return (
     <>
       <AppNavBar/>
+      <MemeList/>
     </>
   )
 }
