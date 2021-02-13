5a8d93aa5faf375ca789be8fdc33600178448688
Asim Shrestha <ashresth@sfu.ca>
2021-02-13 11:54:32 -0800

    🎨 Add ability to create meme

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,11 @@
 import AppNavBar from '../components/AppNavBar';
+import MemeGenerator from '../components/MemeGenerator';
 
 export default function Home() {
   return (
     <div>
       <AppNavBar/>
+      <MemeGenerator/>
     </div>
   )
 }
