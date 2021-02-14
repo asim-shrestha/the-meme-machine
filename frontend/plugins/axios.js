157bcd69898f2b234ee2ba561bd8986999cd8c70
asim-shrestha <asim.shrestha@hotmail.com>
2021-02-14 10:17:36 -0800

    :sparkles: hardcode backend

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
-console.log("api", process.env.NEXT_PUBLIC_API_URL)
+const API_URL = process.env.NODE_ENV === 'production' ? "bubble-bot.com" : 'http://localhost:5000';
 //TODO: USE ENV_VAR
 export const $axios = axios.create({
     baseURL: API_URL
