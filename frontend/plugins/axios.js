da7648b437f8105a03c5f36d8f50443ab21ab4a1
asim-shrestha <asim.shrestha@hotmail.com>
2021-02-14 10:28:34 -0800

    :bug: fix url

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NODE_ENV === 'production' ? "bubble-bot.com" : 'http://localhost:5000';
+const API_URL = process.env.NODE_ENV === 'production' ? "https://bubble-bot.com/" : 'http://localhost:5000';
 //TODO: USE ENV_VAR
 export const $axios = axios.create({
     baseURL: API_URL
